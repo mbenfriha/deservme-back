47668d86b03ccae913a713882f97f1193e31f84c
mohamed.benfriha@epitech.eu <mohamed.benfriha@epitech.eu@epitech.eu>
2019-11-11 03:04:46 +0100

    update facebook login

--- app.js
+++ app.js
@@ -514,6 +514,8 @@ app.get('/auth/facebook',
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', { failureRedirect: urlFront }),
     function(req, res) {
+        console.log(req.session.redirectTo)
+
         // Successful authentication, redirect home.
         if(req.session.redirectTo == '/login')
             res.redirect(admin+req.session.redirectTo+'?id='+req.user.facebook.id);
